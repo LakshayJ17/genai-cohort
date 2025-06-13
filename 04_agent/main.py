597a1fba8262ca827f827a03abb395c730e9e6b0
lakshay17 <lakshayjain01lj@gmail.com>
2025-06-13 17:08:58 +0000

    c3

--- 04_agent/main.py
+++ 04_agent/main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-from datetime import datetime
 import json
 import requests
 
@@ -13,9 +12,11 @@ client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
+
 def run_command(cmd: str):
     result = os.system(cmd)
     return result
+
 
 def get_weather(city: str):
     url = f"https://wttr.in/{city}?format=%C+%t"
@@ -23,7 +24,7 @@ def get_weather(city: str):
 
     if response.status_code == 200:
         return f"The weather in {city} is {response.text}."
-    
+
     return "Something went wrong"
 
 
@@ -33,7 +34,7 @@ available_tools = {
 }
 
 # Insert the current date and time into the system prompt
-SYSTEM_PROMPT = f"""
+SYSTEM_PROMPT = """
     You are an helpfull AI Assistant who is specialized in resolving user query.
     You work on start, plan, action, observe mode.
 
@@ -48,12 +49,12 @@ SYSTEM_PROMPT = f"""
     - Carefully analyse the user query
 
     Output JSON Format:
-    {{
+    {
         "step": "string",
         "content": "string",
         "function": "The name of function if the step is action",
         "input": "The input parameter for the function",
-    }}
+    }
 
     Available Tools:
     - "get_weather": Takes a city name as an input and returns the current weather for the city
@@ -61,22 +62,22 @@ SYSTEM_PROMPT = f"""
 
     Example:
     User Query: What is the weather of new york?
-    Output: {{ "step": "plan", "content": "The user is interested in weather data of new york" }}
-    Output: {{ "step": "plan", "content": "From the available tools I should call get_weather" }}
-    Output: {{ "step": "action", "function": "get_weather", "input": "new york" }}
-    Output: {{ "step": "observe", "output": "12 Degree Cel" }}
-    Output: {{ "step": "output", "content": "The weather for new york seems to be 12 degrees." }}
+    Output: { "step": "plan", "content": "The user is interested in weather data of new york" }
+    Output: { "step": "plan", "content": "From the available tools I should call get_weather" }
+    Output: { "step": "action", "function": "get_weather", "input": "new york" }
+    Output: { "step": "observe", "output": "12 Degree Cel" }
+    Output: { "step": "output", "content": "The weather for new york seems to be 12 degrees." }
 
 """
-# Message array - first - system prompt 
+# Message array - first - system prompt
 messages = [
-  { "role": "system", "content": SYSTEM_PROMPT }
+    {"role": "system", "content": SYSTEM_PROMPT}
 ]
 
 while True:
     # User input and append it to array
     query = input("> ")
-    messages.append({ "role": "user", "content": query })
+    messages.append({"role": "user", "content": query})
 
     # AI call
     while True:
@@ -86,11 +87,13 @@ while True:
             messages=messages
         )
 
-        messages.append({ "role": "assistant", "content": response.choices[0].message.content })
+        messages.append(
+            {"role": "assistant", "content": response.choices[0].message.content})
         parsed_response = json.loads(response.choices[0].message.content)
 
         # Always treat parsed_response as a list of steps
-        steps = parsed_response if isinstance(parsed_response, list) else [parsed_response]
+        steps = parsed_response if isinstance(
+            parsed_response, list) else [parsed_response]
 
         for step in steps:
             if step.get("step") == "plan":
@@ -103,7 +106,8 @@ while True:
                 print(f"🛠️: Calling Tool:{tool_name} with input {tool_input}")
                 if available_tools.get(tool_name) != False:
                     output = available_tools[tool_name](tool_input)
-                    messages.append({ "role": "user", "content": json.dumps({ "step": "observe", "output": output }) })
+                    messages.append({"role": "user", "content": json.dumps(
+                        {"step": "observe", "output": output})})
                 continue
 
             if step.get("step") == "output":
