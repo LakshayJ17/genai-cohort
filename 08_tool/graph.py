8198039f19f2b2a21213e0003b3d1ae92453c4a3
lakshay17 <lakshayjain01lj@gmail.com>
2025-06-17 18:35:46 +0000

    C8

--- 08_tool/graph.py
+++ 08_tool/graph.py
@@ -1,22 +1,44 @@
-#  flake8: noqa
-
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph, START, END\
+from langgraph.graph import StateGraph, START, END
+from langchain.chat_models import init_chat_model
 from langchain_core.tools import tool
+import requests
 
 
+# This makes a tool for langchain - Give tool description in """ """
 @tool
 def get_weather(city: str):
-    
+    """This tool returns the weather data about the given city"""
+
+    url = f"https://wttr.in/{city}?format=%C+%t"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return f"The weather in {city} is {response.text}"
+
+    return "Something went wrong"
+
+
+# Define tools array containing all our tools
+tools = [get_weather]
+
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
+llm = init_chat_model(model_provider="openai", model="gpt-4.1")
+
+
+# Pass tools to llm
+llm_with_tools = llm.bind_tools(tools)
+
+
 def chatbot(state: State):
-    pass
+    message = llm_with_tools.invoke(state["messages"])
+    return {"messages": [message]}
 
 
 graph_builder = StateGraph(State)
@@ -28,11 +50,18 @@ graph_builder.add_edge("chatbot", END)
 
 graph = graph_builder.compile()
 
+
 def main():
     user_query = input("> ")
-    
-    state = State(
-        messages=[{"role" : "user", "content": user_query}]
+
+    _state = State(
+        messages=[{"role": "user", "content": user_query}]
     )
-    
-    
+
+    for event in graph.stream(_state, stream_mode="values"):
+        # Shows only last message
+        if "messages" in event:
+            event["messages"][-1].pretty_print()
+
+
+main()
